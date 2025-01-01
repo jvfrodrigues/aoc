e31ee5f4d11c9997ae650469955ec567d4aec170
Joao Rodrigues <7799029+jvfrodrigues@users.noreply.github.com>
2025-01-01 13:43:44 -0300

    feat: optimize day 9 by using heap

--- 2024/golang/internal/day09/day09.go
+++ 2024/golang/internal/day09/day09.go
@@ -3,6 +3,8 @@ package day09
 import (
 	"aoc/internal/domain"
 	"aoc/internal/utils"
+	"container/heap"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -24,6 +26,12 @@ type DataBlock struct {
 	Size int
 }
 
+type FileBlock struct {
+	ID    int
+	Size  int
+	Start int
+}
+
 func PartOne() int {
 	dataBlocks := readLines(lines)
 	compactData(dataBlocks)
@@ -38,16 +46,16 @@ func PartOne() int {
 }
 
 func PartTwo() int {
-	dataBlocks, emptyBlocks := readLinesStruct(lines)
-	compactDefragedDataBlocks := compactDefragmentedDataBack(dataBlocks, emptyBlocks)
+	fileBlocks, emptyHeaps := readLinesStruct(lines)
+	compactDefragmentedData(fileBlocks, emptyHeaps)
 	result := 0
-	totalIdx := 0
-	for _, dataBlock := range compactDefragedDataBlocks {
-		for _ = range dataBlock.Size {
-			if dataBlock.ID != -1 {
-				result += totalIdx * dataBlock.ID
+	for _, fileBlock := range fileBlocks {
+		if fileBlock.ID != -1 {
+			idx := fileBlock.Start
+			for i := 0; i < fileBlock.Size; i++ {
+				result += fileBlock.ID * idx
+				idx++
 			}
-			totalIdx++
 		}
 	}
 	return result
@@ -75,70 +83,49 @@ func compactData(dataBlocks []int) {
 	}
 }
 
-func compactDefragmentedData(dataBlocks []DataBlock, emptyBlocks map[int][]int) []DataBlock {
-	idxRight := len(dataBlocks) - 1
-	compactDefrag := make([]DataBlock, 0)
-	compactDefrag = append(compactDefrag, dataBlocks...)
-	for idxRight > 0 {
-		currRight := compactDefrag[idxRight]
-		if currRight.ID == -1 {
-			idxRight--
+func compactDefragmentedData(fileBlocks []FileBlock, emptyHeaps []domain.IntHeap) {
+	idx := len(fileBlocks) - 1
+	for idx > 0 {
+		curr := fileBlocks[idx]
+		if curr.ID == -1 {
+			idx--
 			continue
 		}
-		currSize := currRight.Size
-		for key, arr := range emptyBlocks {
-			if key < currSize {
+		heapIndex, emptySpaceIndex := -1, math.MaxInt
+		for i := curr.Size; i < 10; i++ {
+			if len(emptyHeaps[i]) == 0 {
 				continue
 			}
-			if len(arr) == 0 {
+			newEmptySpaceIndex := heap.Pop(&emptyHeaps[i]).(int)
+
+			if newEmptySpaceIndex > curr.Start || newEmptySpaceIndex > emptySpaceIndex {
+				heap.Push(&emptyHeaps[i], newEmptySpaceIndex)
 				continue
 			}
-			first := arr[0]
-			if first >= idxRight {
-				continue
+
+			if heapIndex != -1 {
+				heap.Push(&emptyHeaps[heapIndex], emptySpaceIndex)
 			}
-			emptyBlocks[key] = emptyBlocks[key][1:]
-			sizeDiff := key - currRight.Size
-			if sizeDiff == 0 {
-				compactDefrag[first], compactDefrag[idxRight] = compactDefrag[idxRight], compactDefrag[first]
-			} else {
-				compactDefrag[first].Size = compactDefrag[idxRight].Size
-				compactDefrag[first], compactDefrag[idxRight] = compactDefrag[idxRight], compactDefrag[first]
-				idxRight++
-				first++
-				newElement := DataBlock{
-					ID:   -1,
-					Size: sizeDiff,
-				}
-				compactDefrag = append(compactDefrag[:first], append([]DataBlock{newElement}, compactDefrag[first:]...)...)
-				emptyBlocks[sizeDiff] = append([]int{first}, emptyBlocks[sizeDiff]...)
-			}
-			curr := compactDefrag[idxRight]
-			adjLeft := idxRight - 1
-			adjRight := idxRight + 1
-			if adjRight < len(compactDefrag) {
-				adj := compactDefrag[adjRight]
-				if adj.ID == -1 {
-					curr.Size = curr.Size + adj.Size
-					compactDefrag = append(compactDefrag[:idxRight], append([]DataBlock{curr}, compactDefrag[adjRight+1:]...)...)
-				}
-			}
-			if adjLeft > -1 {
-				adj := compactDefrag[adjLeft]
-				if adj.ID == -1 {
-					curr.Size = curr.Size + adj.Size
-					compactDefrag = append(compactDefrag[:adjLeft], append([]DataBlock{curr}, compactDefrag[adjRight:]...)...)
-				}
-			}
-			emptyBlocks = recheckPositions(compactDefrag)
-			break
+
+			emptySpaceIndex = newEmptySpaceIndex
+			heapIndex = i
 		}
-		idxRight--
+
+		if heapIndex == -1 {
+			idx--
+			continue
+		}
+		fileBlocks[idx].Start = emptySpaceIndex
+
+		if heapIndex > curr.Size {
+			newHeapIndex := heapIndex - curr.Size
+			heap.Push(&emptyHeaps[newHeapIndex], emptySpaceIndex+curr.Size)
+		}
+		idx--
 	}
-	return compactDefrag
 }
 
-func compactDefragmentedDataBack(dataBlocks []DataBlock, emptyBlocks map[int][]int) []DataBlock {
+func compactDefragmentedDataNotOptimized(dataBlocks []DataBlock, emptyHeaps [][]int) []DataBlock {
 	idxLeft := 0
 	idxRight := len(dataBlocks) - 1
 	compactDefrag := make([]DataBlock, 0)
@@ -226,11 +213,15 @@ func readLines(lines []string) []int {
 	return dataBlocks
 }
 
-func readLinesStruct(lines []string) ([]DataBlock, map[int][]int) {
+func readLinesStruct(lines []string) ([]FileBlock, []domain.IntHeap) {
 	fileFlag := false
-	dataBlocks := make([]DataBlock, 0)
-	availableBlocks := make(map[int][]int)
+	fileBlocks := make([]FileBlock, 0)
+	emptyHeaps := make([]domain.IntHeap, 10)
+	for i, _ := range emptyHeaps {
+		heap.Init(&emptyHeaps[i])
+	}
 	incID := 0
+	totalIndex := 0
 	for _, line := range lines {
 		trimmedLine := strings.TrimSpace(line)
 		for _, char := range trimmedLine {
@@ -243,39 +234,17 @@ func readLinesStruct(lines []string) ([]DataBlock, map[int][]int) {
 				continue
 			}
 			if fileFlag {
-				dataBlocks = append(dataBlocks, DataBlock{
-					ID:   incID,
-					Size: int(fileSize),
+				fileBlocks = append(fileBlocks, FileBlock{
+					ID:    incID,
+					Size:  int(fileSize),
+					Start: totalIndex,
 				})
 				incID++
 			} else {
-				dataBlocks = append(dataBlocks, DataBlock{
-					ID:   -1,
-					Size: int(fileSize),
-				})
-				_, ok := availableBlocks[int(fileSize)]
-				if !ok {
-					availableBlocks[int(fileSize)] = make([]int, 0)
-				}
-				availableBlocks[int(fileSize)] = append(availableBlocks[int(fileSize)], len(dataBlocks)-1)
+				heap.Push(&emptyHeaps[fileSize], totalIndex)
 			}
+			totalIndex += int(fileSize)
 		}
 	}
-	return dataBlocks, availableBlocks
-}
-
-func recheckPositions(dataBlocks []DataBlock) map[int][]int {
-	availableBlocks := make(map[int][]int)
-	for idx, block := range dataBlocks {
-		if block.ID != -1 {
-			continue
-		}
-		blockSize := block.Size
-		_, ok := availableBlocks[blockSize]
-		if !ok {
-			availableBlocks[blockSize] = make([]int, 0)
-		}
-		availableBlocks[blockSize] = append(availableBlocks[blockSize], idx)
-	}
-	return availableBlocks
+	return fileBlocks, emptyHeaps
 }
